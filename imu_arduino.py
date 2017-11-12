336509891dfb3efc176b4913144f3dc72f9949e0
Eilidh Southren <eilidhsouthren@gmail.com>
2017-11-12 04:40:50 +0000

    eslam's magic maths

--- imu_arduino.py
+++ imu_arduino.py
@@ -28,11 +28,12 @@ def resize(width, height):
 
 
 
-def read_data(previous_time):
+def read_data(previous_time, prev_x_acc, prev_y_acc, vx, vy):
     global ax, ay, az
     ax = ay = az = 0.0
-    line_done = 0
 
+    prev_vx = vx
+    prev_vy = vy
     # request data by sending a dot
     ser.write(b".")
     # while not line_done:
@@ -51,11 +52,15 @@ def read_data(previous_time):
         ay = (angles[1])
 
         az = (angles[2])
-        print((ax))
-        print((ay))
-        print((az))
+
+
+
+        acc_x = float(ax) * 9.81 / 8192
+        acc_y = float(ay) * 9.81 / 8192
+        print("Acc. x: {}".format(float(acc_x)))
+        print("Acc: y: {}".format(float(acc_y)))
         if previous_time == None:
-            diff = 0
+            diff = today
         else:
             diff = today - previous_time
 
@@ -63,17 +68,40 @@ def read_data(previous_time):
         previous_time = today
 
         splitty = str(diff).split('.')
-        print(splitty[-1])
+        diff = int(splitty[-1])
+        print(diff)
         previous_time = today
 
-    return previous_time
+        # Let's calc the velocity
+
+        #print("Vel.X: {}".format(vx))
+        #print("Vel.Y: {}".format(vy))
+
+
+        #print("diff: {}".format(diff))
+        # v = 0.5 * diff * change in acceleration ( /1000/1000 to convert to seconds)
+        #print("vx: {}".format(vx))
+        if abs(float(ax)) > 0.1 and abs(float(ay)) > 0.1 and abs(float(az)) > 0.1:
+            vx = 0.5 * (diff/1000/1000) * (float(acc_x) + float(prev_x_acc))
+                                    # current value of X.acc
+            vy = 0.5 * (diff/1000/1000) * (float(acc_y) + float(prev_y_acc))
+            prev_x_acc = acc_x
+            prev_y_acc = acc_y
+            print("Vel X: {}".format(vx))
+            print("Vel Y: {}".format(vy))
+    return previous_time, prev_x_acc, prev_y_acc, vx, vy
 
 
 def main():
     global yaw_mode
     previous_time = None
+    vx = 0
+    vy = 0
+    prev_y_acc = 0
+    prev_x_acc = 0
+
     for i in range(10000):
-        previous_time = read_data(previous_time)
+        previous_time, prev_x_acc, prev_y_acc, vx, vy = read_data(previous_time, prev_x_acc, prev_y_acc, vx, vy)
 
 
     ser.close()
